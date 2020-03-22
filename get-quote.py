4860ae62db2efea3b93fb61a1990d592f83c363a
umesh47 <071bel2014@gmail.com>
2020-03-22 21:41:55 +0545

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def primary():
   print("Keep it logically awesome.") 
   f = open("quotes.txt")
   quotes = f.readlines()
@@ -6,4 +6,4 @@ def main():
   print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
