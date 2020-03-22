d311bd81c8f4499827f25d29091c5e2de3e5f290
umesh47 <071bel2014@gmail.com>
2020-03-22 22:19:20 +0545

    Adding quotes from the user

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,23 @@
 import random
 
 def primary():
+  #taking data from user and writing in the text file
+  f= open("quotes.txt","a")
+  input1 = input("Type some quote or text")
+  f.write(input1 + "\n")
+  f.close()
+
   #print("Keep it logically awesome.") 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   last=len(quotes)-1
-  rnd=random.randint(0,last)
-  print(quotes[rnd])
+  print(quotes[last])
+  
+  #rnd=random.randint(0,last)
+  #print(quotes[rnd].rstrip("\n"))
+  #rnd=random.randint(0,last)
+  #print(quotes[rnd].rstrip("\n"))
 
 if __name__== "__main__":
   primary()
