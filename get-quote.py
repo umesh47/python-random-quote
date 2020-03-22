1de34e56ea684dad5823fb168199496c0e516fec
umesh47 <071bel2014@gmail.com>
2020-03-22 21:29:11 +0545

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,9 @@
 def main():
-  # print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  print("Keep it logically awesome.") 
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print(quotes)
 
 if __name__== "__main__":
   main()
